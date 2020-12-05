f8078e22b9e431dfff14a274d3af1f5308a9e244
Adrian Farnell <adrianjfarnell@gmail.com>
2020-12-05 15:30:59 +0000

    pre commenting

--- Program.cs
+++ Program.cs
@@ -27,10 +27,6 @@ namespace yaml
                 {
                     content = reader.ReadToEnd();
 
-                    //Console.WriteLine("content:" + content);
-
-                    string val = "customer";
-
                     var deserializer = new Deserializer();
                     var yamlObject = deserializer.Deserialize<dynamic>(content);
 
@@ -39,38 +35,6 @@ namespace yaml
                     Console.WriteLine(yamlObject["date"]);
 
                 }
-
-
-                /*/ Open the text file using a stream reader.
-                using (var sr = new StreamReader(yamlFileName))
-                {
-                    // Read the stream as a string, and write the string to the console.
-                    //Console.WriteLine(sr.ReadToEnd()) ;
-                    //var yaml = new YamlStream();
-                    //yaml.Load(sr);
-
-                    Console.WriteLine();
-
-                    var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-
-                    var deserializer = new Deserializer();
-                    var yamlObject = deserializer.Deserialize<dynamic>(sr)["receipt"]["customer"].Values;
-
-                    string doodad = JsonSerializer.Serialize(yamlObject);
-
-                    Console.WriteLine(doodad);
-                    
-
-                    //var serializer = new JsonSerializer();
-                    //serializer.Serialize(Console.Out, yamlObject);
-
-
-                    foreach (var entry in mapping.Children)
-                    {
-                        Console.WriteLine(((YamlScalarNode)entry.Key).Value);
-                    }
-                    */
-                
  
             }
             catch (IOException e)
@@ -78,9 +42,6 @@ namespace yaml
                 Console.WriteLine(" Error: The file could not be read:");
                 Console.WriteLine(e.Message);
             }
-
-            //Console.WriteLine (" \r\n File Contents:- \r\n" +  yamlStream);
-
 
             Console.WriteLine ("\r\n"); 
 
