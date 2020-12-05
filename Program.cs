11441214fd0f60b975ea5c99bf657c8f25d84fed
Adrian Farnell <adrianjfarnell@gmail.com>
2020-12-05 15:28:58 +0000

    first time I can see whats going on using yamldotnet

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using YamlDotNet.Core;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 using YamlDotNet.RepresentationModel;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace yaml
 {
@@ -14,36 +16,61 @@ namespace yaml
 
             string yamlFileName = "";
             Console.WriteLine("\r\n == yaml Check == (c) MMXX");
-            //read command line arguments for file type and output test results
-            if ( args.Length < 1 ){
-                Console.WriteLine (" Err: No Args \r\n");
-                displayHelp();
-                Environment.Exit(0);
-            } else {
-                Console.WriteLine(" Getting filename");
-                yamlFileName = args[0].Replace("--filename=", "");
-            }
-            
-            Console.WriteLine ( "\r\n arg: " + yamlFileName);
+
+
+            yamlFileName = "sample.yaml";
 
             try
             {
-                // Open the text file using a stream reader.
+                string content;
+                using (StreamReader reader = new StreamReader(yamlFileName))
+                {
+                    content = reader.ReadToEnd();
+
+                    //Console.WriteLine("content:" + content);
+
+                    string val = "customer";
+
+                    var deserializer = new Deserializer();
+                    var yamlObject = deserializer.Deserialize<dynamic>(content);
+
+                    Console.WriteLine(yamlObject["items"][0]["part_no"]);
+                    Console.WriteLine(yamlObject["items"][1]["part_no"]);
+                    Console.WriteLine(yamlObject["date"]);
+
+                }
+
+
+                /*/ Open the text file using a stream reader.
                 using (var sr = new StreamReader(yamlFileName))
                 {
                     // Read the stream as a string, and write the string to the console.
                     //Console.WriteLine(sr.ReadToEnd()) ;
-                    var yaml = new YamlStream();
-                    yaml.Load(sr);
+                    //var yaml = new YamlStream();
+                    //yaml.Load(sr);
+
+                    Console.WriteLine();
 
                     var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+
+                    var deserializer = new Deserializer();
+                    var yamlObject = deserializer.Deserialize<dynamic>(sr)["receipt"]["customer"].Values;
+
+                    string doodad = JsonSerializer.Serialize(yamlObject);
+
+                    Console.WriteLine(doodad);
+                    
+
+                    //var serializer = new JsonSerializer();
+                    //serializer.Serialize(Console.Out, yamlObject);
+
 
                     foreach (var entry in mapping.Children)
                     {
                         Console.WriteLine(((YamlScalarNode)entry.Key).Value);
                     }
-
-                }
+                    */
+                
  
             }
             catch (IOException e)
