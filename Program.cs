c5ce5e3a07a96bdba4055df448451016558c06c7
Adrian Farnell <adrianjfarnell@gmail.com>
2020-12-05 15:35:43 +0000

    comments added.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.IO;
-using YamlDotNet.Core;
 using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
-using YamlDotNet.RepresentationModel;
-using System.Text.Json;
-using System.Collections.Generic;
 
 namespace yaml
 {
@@ -13,23 +8,33 @@ namespace yaml
     {
         static void Main(string[] args)
         {
-
+            //setup strings for file name
             string yamlFileName = "";
+
+            //bit of UI
             Console.WriteLine("\r\n == yaml Check == (c) MMXX");
 
-
+            //hardcoded filename, TODO: will replace with args collector. 
             yamlFileName = "sample.yaml";
 
             try
             {
+                //setup string for content to work on
                 string content;
+
+                //read the contents of the specified file into variable 'content'
                 using (StreamReader reader = new StreamReader(yamlFileName))
                 {
+                    //read the file to the end
                     content = reader.ReadToEnd();
 
+                    //setup the yaml deserializer
                     var deserializer = new Deserializer();
+                    
+                    //get the yaml into an object we can start reading programmatically. 
                     var yamlObject = deserializer.Deserialize<dynamic>(content);
 
+                    //some display entries
                     Console.WriteLine(yamlObject["items"][0]["part_no"]);
                     Console.WriteLine(yamlObject["items"][1]["part_no"]);
                     Console.WriteLine(yamlObject["date"]);
